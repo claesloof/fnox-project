069f4b5ef32a797f8d9e06a436cbd299a7e0a21e
Claes Loof <loofclaes@gmail.com>
2018-09-21 15:02:55 +0200

    Implemented tests for Java Rest Service: get() and post().

--- backend/src/main/java/boxinator/rest/service/resources/BoxResource.java
+++ backend/src/main/java/boxinator/rest/service/resources/BoxResource.java
@@ -1,17 +1,11 @@
 package boxinator.rest.service.resources;
 
-import java.net.URI;
 import java.util.List;
-import java.util.Map;
 
 import boxinator.rest.service.entity.Box;
 import boxinator.rest.service.repository.BoxRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.print.attribute.standard.Destination;
 
 @RestController
 public class BoxResource {
@@ -21,13 +15,13 @@ public class BoxResource {
 
     @CrossOrigin
     @GetMapping("/boxes")
-    public List<Box> retrieveAllBoxes() {
+    public List<Box> get() {
         return boxRepository.findAll();
     }
 
     @CrossOrigin
     @PostMapping("/boxes")
-    public Box createBox(@RequestBody Box box) {
+    public Box post(@RequestBody Box box) {
         return boxRepository.save(box);
     }
 }
--- backend/src/test/java/boxinator/rest/service/ApplicationTests.java
+++ backend/src/test/java/boxinator/rest/service/ApplicationTests.java
@@ -1,16 +1,88 @@
 package boxinator.rest.service;
 
+import boxinator.rest.service.entity.Box;
+import boxinator.rest.service.repository.BoxRepository;
+import boxinator.rest.service.resources.BoxResource;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Collections.singletonList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(BoxResource.class)
 public class ApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
+	@MockBean
+	private BoxResource boxResource;
+
+	/**
+	 * Performs a mock get request
+	 * Creates a singleton list with a Box that will be returned
+	 * Asserts if response has an item
+	 * Asserts if item in response has name that was declared
+	 * @throws Exception
+	 */
 	@Test
-	public void contextLoads() {
+	public void getBoxes() throws Exception {
+		Box box = new Box();
+		box.setName("test");
+		List<Box> boxes = singletonList(box);
+
+		given(boxResource.get()).willReturn(boxes);
+
+		mvc.perform(get("/boxes")
+				.contentType(APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$", hasSize(1)))
+				.andExpect(jsonPath("$[0].name", is(box.getName())));
+	}
+
+	/**
+	 * Performs a mock post request with an empty box (currently acceptable)
+	 * Asserts if OK status
+	 * @throws Exception
+	 */
+	@Test
+	public void postBox() throws Exception {
+		Box box = new Box();
+		ObjectMapper objectMapper = new ObjectMapper();
+		String json = objectMapper.writeValueAsString(box);
+
+
+		this.mvc.perform(post("/boxes")
+				.contentType(APPLICATION_JSON)
+				.content(json))
+				.andExpect(status().isOk());
+
 	}
 
 }
